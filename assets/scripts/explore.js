762f8531e01df3554b8a1d5fe38e51c7ba3cc71e
spadmanaban25 <gsspadmanaban@gmail.com>
2023-05-07 18:19:33 -0700

    Explore Section

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,44 @@
-// explore.js
+//explore.js
 
 window.addEventListener('DOMContentLoaded', init);
 
+const synth = window.speechSynthesis;
+
 function init() {
-  // TODO
+  // on page load, initialize all available speech voices
+  var select = document.getElementById('voice-select');
+  window.addEventListener('load', function() {
+    let voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = voices[i].name;
+      select.appendChild(option);
+    }
+  });
+
+  
+  var playSpeechButton = document.querySelector('button');
+  playSpeechButton.addEventListener('click', function() {
+    var inputTxt = document.querySelector('textarea');
+    var selectedVoice = select.value;
+    const speak = new SpeechSynthesisUtterance(inputTxt.value);
+    let voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name == selectedVoice) {
+        speak.voice = voices[i];
+      }
+    }
+
+    synth.speak(speak);
+    
+    var synthImg = document.querySelector('img');
+
+    speak.onstart = function() {
+      synthImg.src = 'assets/images/smiling-open.png';
+    }
+
+    speak.onend = function() {
+      synthImg.src = 'assets/images/smiling.png';
+    }
+  });
 }
