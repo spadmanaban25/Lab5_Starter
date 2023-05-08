70941a846dcb725c4ac67c8ea6d306dde7999fb4
spadmanaban25 <gsspadmanaban@gmail.com>
2023-05-07 19:29:01 -0700

    Explore Section

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -6,18 +6,20 @@ const synth = window.speechSynthesis;
 
 function init() {
   var select = document.getElementById('voice-select');
-  window.addEventListener('load', function() {
-    let voices = synth.getVoices();
-    for (let i = 0; i < voices.length; i++) {
-      const option = document.createElement('option');
-      option.textContent = voices[i].name;
-      select.appendChild(option);
-    }
+  window.addEventListener('load', function () {
+    synth.onvoiceschanged = function () {
+      let voices = synth.getVoices();
+      for (let i = 0; i < voices.length; i++) {
+        const option = document.createElement('option');
+        option.textContent = voices[i].name;
+        select.appendChild(option);
+      }
+    };
   });
 
-  
+
   var playSpeechButton = document.querySelector('button');
-  playSpeechButton.addEventListener('click', function() {
+  playSpeechButton.addEventListener('click', function () {
     var inputTxt = document.querySelector('textarea');
     var selectedVoice = select.value;
     const speak = new SpeechSynthesisUtterance(inputTxt.value);
@@ -29,14 +31,14 @@ function init() {
     }
 
     synth.speak(speak);
-    
+
     var synthImg = document.querySelector('img');
 
-    speak.onstart = function() {
+    speak.onstart = function () {
       synthImg.src = 'assets/images/smiling-open.png';
     }
 
-    speak.onend = function() {
+    speak.onend = function () {
       synthImg.src = 'assets/images/smiling.png';
     }
   });
